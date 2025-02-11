d23a85e51a2fef9c4c01a239e441c7ed57477bba
deva <deva.thevaranchan@gmail.com>
2025-02-11 16:14:11 +0100

    Endpoint Hello World

--- MemoryDB/src/main/java/com/ARYD/MemoryDB/controller/tables.java
+++ MemoryDB/src/main/java/com/ARYD/MemoryDB/controller/tables.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/tables")
 public class tables {
 
     @GetMapping("/Hello")
     public String Hello(){
-        return "Hello World ! ";
+        return "Hello RAQYD";
     }
+
+
 }
